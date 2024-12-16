8941fb15e049e78609d666703d5cfa904291ec66
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-12-16 18:19:59 +0800

    getneighbors method by brute force <3 <3

--- src/components/getNeighbors.js
+++ src/components/getNeighbors.js
@@ -1,8 +1,33 @@
 import stringToArr from "./stringToArr.js";
 export default function getNeighbors(vertex) {
+  const neighbors = [];
   const [x, y] = stringToArr(vertex);
-  console.log(x, y);
-  // return stringToArr(vertex);
+  // look at this horror masterpiece
+  if (x - 1 >= 0 && y + 2 <= 7) {
+    neighbors.push([x - 1, y + 2]);
+  }
+  if (x - 1 >= 0 && y - 2 >= 0) {
+    neighbors.push([x - 1, y - 2]);
+  }
+  if (x + 1 <= 7 && y + 2 <= 7) {
+    neighbors.push([x + 1, y + 2]);
+  }
+  if (x + 1 <= 7 && y - 2 >= 0) {
+    neighbors.push([x + 1, y - 2]);
+  }
+  if (x - 2 >= 0 && y + 1 <= 7) {
+    neighbors.push([x - 2, y + 1]);
+  }
+  if (x - 2 >= 0 && y - 1 >= 0) {
+    neighbors.push([x - 2, y - 1]);
+  }
+  if (x + 2 <= 7 && y + 1 <= 7) {
+    neighbors.push([x + 2, y + 1]);
+  }
+  if (x + 2 <= 7 && y - 1 >= 0) {
+    neighbors.push([x + 2, y - 1]);
+  }
+  return neighbors;
 }
 
-// console.log(getNeighbors("[0, 0]"));
+console.log(getNeighbors("[7, 7]"));
